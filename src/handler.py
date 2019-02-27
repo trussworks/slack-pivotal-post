11f57ed5accb78b3de48aa4a2e6eaf87b2d3f62a
ralren <delosreyes.ren@gmail.com>
2019-02-27 14:57:01 -0500

    Modify handler.py to autotag requester

--- src/handler.py
+++ src/handler.py
@@ -3,6 +3,7 @@ import html
 import logging
 import os
 import re
+from slackclient import SlackClient
 from urllib.parse import parse_qs
 
 import boto3
@@ -18,6 +19,7 @@ pivotal_headers = {
 }
 pivotal_url = "https://www.pivotaltracker.com/services/v5"
 story_post_url = pivotal_url + "/projects/{project_id}/stories"
+project_membership_url = pivotal_url + "/projects/{project_id}/memberships"
 pivotal_project_pattern = re.compile(r"pivotaltracker\.com/n/projects/([\d]+)")
 pivotal_error_codes = ("unauthorized_operation", "unfound_resource")
 
@@ -123,6 +125,7 @@ class SlashCommandRequest():
         self.text = params.get("text", [""])[0]
         self.token = params["token"][0]
         self.user = params["user_name"][0]
+        self.user_id = params["user_id"][0]
         self.channel_id = params["channel_id"][0]
         self.channel_name = params["channel_name"][0]
 
@@ -161,19 +164,19 @@ def get_channel_pairing(channel):
 
 
 def get_project_name(project_id):
-        """Returns a JSON response object describing a Pivotal project"""
-        logger.info("Querying Pivotal project info")
-        project_info = requests.get(pivotal_url + "/projects/" + project_id,
-                                    headers=pivotal_headers).json()
+    """Returns a JSON response object describing a Pivotal project"""
+    logger.info("Querying Pivotal project info")
+    project_info = requests.get(pivotal_url + "/projects/" + project_id,
+                                headers=pivotal_headers).json()
 
-        if project_info.get("code") in pivotal_error_codes:
-            raise PivotalProjectAccessException()
-        else:
-            return project_info["name"]
+    if project_info.get("code") in pivotal_error_codes:
+        raise PivotalProjectAccessException()
+    else:
+        return project_info["name"]
 
 
 def get_pivotal_project_id(action_body):
-    # Attempts to extract a Pivotal Tracker project ID from a pair command
+    """Attempts to extract a Pivotal Tracker project ID from a pair command"""
     valid_project_url = pivotal_project_pattern.search(action_body)
 
     if valid_project_url:
@@ -208,21 +211,51 @@ def remove_pairing(channel_id, project_name):
         ItemName=channel_id)
 
 
-def post_new_tracker_story(message, project_id, user):
+def post_new_tracker_story(message, project_id, user, user_id):
     """Posts message contents as a story to the bound project."""
     if ";" in message:
         name, description = message.split(";", maxsplit=1)
     else:
         name, description = (message, "")
-    story_name = "{name} (from {user})".format(
-        name=name.strip(), user=user)
+
+    pivotal_user_id = get_pivotal_user_id(project_id, user, user_id)
+    if pivotal_user_id:
+        content = {
+            "name": name.strip(),
+            "description": description.strip(),
+            "requested_by_id": pivotal_user_id}
+    else:
+        story_name = "{name} (from {user})".format(
+            name=name.strip(), user=user)
+        content = {
+            "name": story_name,
+            "description": description.strip(),
+            "requested_by_id": pivotal_user_id}
     response = requests.post(
         story_post_url.format(project_id=project_id),
         headers=pivotal_headers,
-        json={"name": story_name,
-              "description": description.strip()})
+        json=content)
     story_url = response.json()["url"]
     return name, story_url
+
+
+def get_pivotal_user_id(project_id, user, user_id):
+    """Gets pivotal user id for the Slack user"""
+    slack_email = get_slack_email(user_id)
+    # Get all the members of Pivotal project and finds the
+    # Pivotal user id associated with the slack email
+    membership_response = requests.get(project_membership_url, headers=pivotal_headers).format(project_id=project_id)
+    for member in membership_response.json():
+        person = member['person']
+
+        if slack_email == person['email']:
+            return person['id']
+
+
+def get_slack_email(user_id):
+    """Gets the Slack email associated with the Slack user id"""
+    sc = SlackClient(deployed_slack_token)
+    return sc.api_call(method='users.info', user=user_id)['user']['profile']['email']
 
 
 def export_pairings():
@@ -297,7 +330,7 @@ def lambda_handler(event, context):
     # If no other actions were taken then we're posting a new Tracker story
     project_name = get_project_name(paired_tracker_project)
     story_name, story_url = post_new_tracker_story(
-        parsed_request.text, paired_tracker_project, parsed_request.user)
+        parsed_request.text, paired_tracker_project, parsed_request.user, parsed_request.user_id)
     return response(200, SUCCESSFUL_POST_RESPONSE.format(
         story=story_name,
         project=project_name,
